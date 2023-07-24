18d914c50f94313f9b26cbc4cef2b8fb67d345f9
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-07-24 20:07:51 +0600

    details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,9 +11,7 @@ app.use(express.json());
 
 // MONGODB START
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7suhtvy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7suhtvy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,21 +27,40 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const collegeSixCardCollection = client.db("EndGame-Project").collection("collegeSixCard");
-    const collegeThreeCardCollection = client.db("EndGame-Project").collection("collegeThreeCard");
+    const collegeSixCardCollection = client
+      .db("EndGame-Project")
+      .collection("collegeSixCard");
+    const collegeThreeCardCollection = client
+      .db("EndGame-Project")
+      .collection("collegeThreeCard");
 
     // get six card api
-    app.get('/sixCard',  async(req, res)=>{
-        const result = await collegeSixCardCollection.find().toArray();
-        res.send(result)
-      })
+    app.get("/sixCard", async (req, res) => {
+      const result = await collegeSixCardCollection.find().toArray();
+      res.send(result);
+    });
     // get three card api
-    app.get('/threeCard',  async(req, res)=>{
-        const result = await collegeThreeCardCollection.find().toArray();
-        res.send(result)
-      })
+    app.get("/threeCard", async (req, res) => {
+      const result = await collegeThreeCardCollection.find().toArray();
+      res.send(result);
+    });
 
-
+    // collegeSixCard details data
+    app.get("/sixCard/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeSixCardCollection.findOne(query);
+      res.send(result);
+    });
+    // collegeThreeCard details data
+    app.get("/threeCard/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeThreeCardCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
